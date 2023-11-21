f0dc5332b1147ebe94a3ba5f6f48e7127e85fd52
WitherMite <43625516+WitherMite@users.noreply.github.com>
2023-11-20 22:46:36 -0600

    Solve ReverseString
    Didnt read all the string methods lmao

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const characters = [];
+    let length = string.length;
+    let reversed = "";
 
+    for (let i=1; i<=length; i++) {
+        characters.push(string.charAt(length - i));
+    }
+
+    for (let i=0; i<length; i++) {
+        reversed += characters.shift();
+    }
+    return reversed;
 };
 
 // Do not edit below this line
