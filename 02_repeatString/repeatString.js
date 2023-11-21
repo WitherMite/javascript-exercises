44a86294e63e55c359ca0a3a96e74425292b5734
WitherMite <43625516+WitherMite@users.noreply.github.com>
2023-11-20 22:05:47 -0600

    Solve RepeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
 
+    let newString = "";
+
+    for (; num>0; num--) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
