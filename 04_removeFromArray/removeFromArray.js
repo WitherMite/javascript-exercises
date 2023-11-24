29718fce5f5ececcf74330b9a82eb297f7bf3314
WitherMite <43625516+WitherMite@users.noreply.github.com>
2023-11-24 16:47:21 -0600

    Solve RemoveFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeArgs) {
+    let newArray = array
+    for (arg of removeArgs) {
+        while (newArray.indexOf(arg) !== -1) {
+            newArray.splice(newArray.indexOf(arg),1);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
