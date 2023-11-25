d5d9a433b2112b75d92d5176ecd7bc6b50072468
WitherMite <43625516+WitherMite@users.noreply.github.com>
2023-11-24 18:03:30 -0600

    Solve SumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    
+    if (checkError(start) || checkError(end)) {
+        return "ERROR";
+    }
+    if (end < start) {
+        const temp = end;
+        end = start;
+        start = temp;
+    }
 
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+    return sum;
 };
+
+function checkError(num) {
+    if (num < 0 || typeof num !== "number") {
+        return true;
+    }
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
