05619a9120e7d399f0cd3d37a3c1c594d73b6a9f
WitherMite <43625516+WitherMite@users.noreply.github.com>
2023-12-06 08:02:45 -0600

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    badChars = ["!", " ", ".", ","];
+    let word = string.toLowerCase();
 
+    badChars.forEach((badChar) => {
+        word = word.replaceAll(badChar, "");
+    });
+
+    const reverseWord = word.split("").reverse().join("");
+    if (reverseWord === word) {
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
