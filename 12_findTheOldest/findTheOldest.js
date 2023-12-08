23db1cf4cba180ed63b8b83acb018a774e327547
WitherMite <43625516+WitherMite@users.noreply.github.com>
2023-12-08 10:17:48 -0600

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        let oldestMax = oldest.yearOfDeath;
+        if (!oldest.yearOfDeath) oldestMax = new Date().getFullYear();
 
+        let currentMax = current.yearOfDeath;
+        if (!oldest.yearOfDeath) currentMax = new Date().getFullYear();
+
+        const oldestAge = oldestMax - oldest.yearOfBirth;
+        const currentAge = currentMax- current.yearOfBirth;
+
+        return (oldestAge < currentAge) ? current : oldest;
+    }, arr[0]);
 };
 
 // Do not edit below this line
