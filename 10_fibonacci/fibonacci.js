fdd4d570b5d346f8bfc67c5ff8637adf9aa5869c
WitherMite <43625516+WitherMite@users.noreply.github.com>
2023-12-08 09:33:47 -0600

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(posInput) {
+    const pos = Number(posInput);
+    if (pos < 0) return "OOPS";
+    let fib = 0
+    let nextFib = 1
+    for (let i = 0; i < pos; i++) {
+        [fib , nextFib] = [nextFib, fib + nextFib];
+    }
+    return fib;
 };
 
 // Do not edit below this line
